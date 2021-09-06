1bd4bd4bbf1cd5da90641354a77dee9abf7bac7e
reshalfahsi <resha.alfahsi@gmail.com>
2021-09-06 09:23:33 +0700

    trying to add access token and refresh token functionality

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"log"
-	"net/http"     
+	"net/http"
+	"github.com/labstack/echo"
 )
 
 func main() {
 	port := getenv("PORT", "8080")
 
-	http.HandleFunc("/login", Login)
-	// http.HandleFunc("/predict", Predict)
-	http.HandleFunc("/refresh", Refresh)
-	// http.HandleFunc("/logout", Logout)
+	e := echo.New()
+	h := &handler{}
 
-	log.Fatal(http.ListenAndServe(":" + port, nil))
+	e.GET("/", h.index)
+	e.POST("/login", h.login)
+	e.POST("/predict", h.predict, isLoggedIn)
+	e.GET("/admin", h.private, isLoggedIn, isAdmin)
+	e.POST("/token", h.token)
+
+	e.Logger.Fatal(e.Start(":" + port))
 }
